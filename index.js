34d49aea789857bac29586db004bd69121723ee5
ariaan ahmed <you@example.com>
2023-06-13 11:51:26 +0600

    verfify jwt middleware added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if(!authorization){
@@ -14,7 +15,13 @@ const verifyJWT = (req, res, next) => {
   }
   const token = authorization.split(' ')[1];
   
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({error: true, message: 'Unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
 }
 
 
