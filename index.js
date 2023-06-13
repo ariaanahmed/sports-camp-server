0112fc8fd96b7f865a12f9f924a8dd5a086f8f78
ariaan ahmed <you@example.com>
2023-06-13 11:45:50 +0600

    verify jwt added

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'Unauthorized access'})
+  }
+  const token = authorization.split(' ')[1];
+  
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
