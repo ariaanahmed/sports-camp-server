d58f2a662f7ec5eecbd196a8fbe6ca785eb9767b
ariaan ahmed <you@example.com>
2023-06-13 11:37:21 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -30,8 +31,13 @@ async function run() {
     const allClassesCollection = client.db('assignment12').collection('allClasses');
     const bookedClassCollection = client.db('assignment12').collection('bookedClasses');
 
-    // users
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+      res.send({token})
+    })
 
+    // users
     app.get('/users', async(req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
